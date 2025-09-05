67692f8538a7f7236e13d566087aacbe19ad528c
SayliAjgaonkar <sayliajgaonkar99@gmail.com>
2025-09-05 21:41:36 +0530

    Snake_Water_Gun Game
    🐍 Snake Water Gun Game (Python)
    
    A simple Python console game based on the classic "Snake Water Gun" — similar to Rock Paper Scissors. The player plays against the computer in a single round of this fun, random chance game.
    
    🎮 Game Rule
    The logic is as follows:
    * Snake (s) drinks Water (w) → Snake wins ✅
    * Water (w) douses Gun (g) → Water wins ✅
    * Gun (g) kills Snake (s) → Gun wins ✅
    * If both the player and computer choose the same, it's a draw 🤝
    
    🧠 How It Works
    The computer randomly selects one of the three options: Snake (1), Water (-1), or Gun (0).
    The user enters their choice:
    * s for Snake
    * w for Water
    * g for Gun
    The winner is determined using simple conditional logic.
    
    💻 Example Output
              Enter your choice : s
              You chose Snake
              Computer chose Gun
              You lose
    
    🛠️ Technologies Used
    Python 3
    Built-in random module
    
    ✅ Future Improvements
    Input validation for incorrect entries
    Option to play multiple rounds with scoring
    Add a simple GUI using Tkinter
    Refactor code using functions for better readability
    
    📁 File Structure
    snake_water_gun.py
    
    🚀 Getting Started
    Make sure you have Python 3 installed.
    Clone the repo or copy the code into a .py file.
    Run the file: - ( python snake_water_gun.py )
    Follow the prompt to enter your choice.
    
    🧑‍💻 Author
    Sayli Ajgaonkar
    Beginner Python Developer | Exploring Fun Projects 🚀

---
+++ snake_water_gun.py
@@ -0,0 +1,42 @@
+import random
+
+'''
+1 for snake
+-1 for water
+0 for gun
+
+'''
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice : ")
+youdict = {"s":1, "w":-1, "g":0}
+reversedict = {1:"Snake", -1:"Water", 0:"Gun"}
+
+you = youdict[youstr]
+
+print(f"You chose {reversedict[you]}\nComputer chose {reversedict[computer]}")
+
+if(computer == you):
+    print("it's a draw")
+    
+else:
+    if(computer == -1 and you == 1):
+        print("You win!")
+    
+    elif(computer == -1 and you == 0):
+        print("You lose")
+    
+    elif(computer == 1 and you == -1):
+        print("You lose")
+        
+    elif(computer == -1 and you == 0):
+        print("You win")
+        
+    elif(computer == 0 and you == -1):
+        print("You win")
+        
+    else:
+        print("You lose")
+    
+    
+        
